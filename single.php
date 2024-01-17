f8c77714af13a4a49c0bf62e84af5e69ca09a63c
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-01-17 21:38:23 +0545

    Comments

--- single.php
+++ single.php
@@ -9,94 +9,48 @@
 
 get_header();
 ?>
- <!-- <aside class="sidebar">
-            <div class="container">
-                <div class="row">
-                    <div class="article-info col-md-3">
-                        <?php $categories = get_the_category();
-                        if (!empty($categories)) : ?>
-                            <div class="posted-in">
-                                <h4><?php _e('Posted In', 'nd_dosth'); ?></h4>
-                                <?php the_category(); ?>
-                            </div>
-                        <?php endif; ?>
-
-                        <div class="published-on">
-                            <h4><?php _e('Publish On', 'nd_dosth'); ?></h4>
-                            <?php the_date(); ?>
+<div class="content-container">
+    <?php while( have_posts() ): ?>
+        <?php the_post(); ?>
+        <?php if ( has_post_thumbnail() ) :
+            $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
+            <div class="full-width-featured-image" style="background-image:url(<?php echo $featured_image[0]; ?>);">
+                <h2><?php the_title(); ?></h2>
+            </div>
+        <?php endif; ?>
+        <div class="container">
+            <div class="row">
+                <div class="article-info col-md-3">
+                    <?php $categories = get_the_category(); ?>
+                    <?php if ( ! empty( $categories ) ) : ?>
+                        <div class="posted-in">
+                            <h4><?php _e( 'Posted In', 'draft' ); ?></h4>
+                            <?php the_category(); ?>
                         </div>
-
-                        <div class="post-author">
-                            <h4><?php _e('Author', 'nd_dosth'); ?></h4>
-                            <a class="author-archive" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>">
-                                <?php the_author(); ?>
-                            </a>
-                            <?php echo get_avatar(get_the_author_meta('ID'), 100); ?>
-                        </div>
+                    <?php endif; ?>
+                    <div class="published-on">
+                        <h4><?php _e( 'Publish On', 'draft' ); ?></h4>
+                        <?php the_date(); ?>
                     </div>
-
-                    <div id="actual-article" class="col-md-8">
-                        <?php the_content(); ?>
+                    <div class="post-author">
+                        <h4><?php _e( 'Author', 'draft' ); ?></h4>
+                        <a class="author-archive" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+                            <?php the_author(); ?>
+                        </a>
+                        <?php echo get_avatar( get_the_author_meta('ID')  , 100 ); ?>
                     </div>
                 </div>
-            </div></aside> -->
-<main id="primary" class="site-main">
+                <div id="actual-article" class="col-md-8">
+                    <?php the_content(); ?>
 
-   
-        <?php
-    set_post_thumbnail_size(10, 10); // Set the thumbnail size to 200x200 pixels
-    while (have_posts()) :
-        the_post();
-        if (has_post_thumbnail()) {
-		}
-  
-		get_template_part('template-parts/content', get_post_type());
-  
-		// Display post navigation
-		?>
-        <div class="row">
-            <?php
-            // Previous Post
-            $prevPost = get_previous_post();
-            if ($prevPost) :
-            ?>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><a href="<?php echo esc_url(get_permalink($prevPost)); ?>"><?php esc_html_e('Previous', 'draft'); ?></a></h5>
-                        <p class="card-text">
-                            <a href="<?php echo esc_url(get_permalink($prevPost)); ?>" class="btn btn-primary"><?php echo esc_html($prevPost->post_title); ?></a>
-                        </p>
-                    </div>
+                    <div class="entry-content">
+                <?php the_excerpt(); ?>
+            </div>
+            <?php comments_template(); ?>
+
                 </div>
             </div>
-            <?php endif; ?>
-
-            <?php
-            // Next Post
-            $nextPost = get_next_post();
-            if ($nextPost) :
-            ?>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><a href="<?php echo esc_url(get_permalink($nextPost)); ?>"><?php esc_html_e('Next', 'draft'); ?></a></h5>
-                        <p class="card-text">
-                            <a href="<?php echo esc_url(get_permalink($nextPost)); ?>" class="btn btn-primary"><?php echo esc_html($nextPost->post_title); ?></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <?php endif; ?>
         </div>
-
-        <?php
-        // If comments are open or we have at least one comment, load up the comment template.
-        if (comments_open() || get_comments_number()) :
-            comments_template();
-        endif;
-
-    endwhile; // End of the loop.
-    ?>
-</main><!-- #main -->
-
+    <?php endwhile; ?>
+</div>
+<?php get_footer(); ?>
