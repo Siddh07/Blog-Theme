5d52580e92819f8c1e876b87fdbd07a5bd11eec8
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-01-19 19:13:41 +0545

    Custom queries

--- index.php
+++ index.php
@@ -7,50 +7,73 @@
  * @package draft
  */
 
-
-
 get_header();
 ?>
 
 <div class="container">
     <div class="row">
         <div class="blog-posts col-md-8">
-        <?php if ( have_posts() ): ?>
-            <?php while( have_posts() ): ?>
-                <?php the_post(); ?>
-                <div class="blog-post">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <?php if ( has_post_thumbnail() ) :
-                        $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
-                        <div class="blog-post-thumb">
-                            <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
+            <?php if ( have_posts() ): ?>
+                <?php while( have_posts() ): ?>
+                    <?php the_post(); ?>
+                    <div class="blog-post">
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <?php if ( has_post_thumbnail() ) :
+                            $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
+                            <div class="blog-post-thumb">
+                                <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
+                            </div>
+                        <?php endif; ?>
+                        <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
+                        
+                        <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More' ); ?></a>
+                        <div class="posted-in">
+                            <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
+                            <span><?php the_category( ', ' ); ?></span>
                         </div>
-                    <?php endif; ?>
-                    <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
-                    
-                    <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More' ); ?></a>
-                    <div class="posted-in">
-                        <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
-                        <span><?php the_category( ', ' ); ?></span>
                     </div>
+                <?php endwhile; ?>
+
                 
-                  
-                </div>
-            <?php endwhile; ?>
-   
-            <?php the_posts_pagination(); ?>
-        <?php else: ?>
-            <p><?php _e( 'No Blog Posts found', 'nd_dosth' ); ?></p>
-        <?php endif; ?>
-     
-        </div>
-       
+                
+                
+                <?php 
+$blog_posts_query = new WP_Query(
+   array(
+       'post_type'     => 'post',
+       'posts_per_page' => 2
+   )  
+);
+?>
+<?php if ( $blog_posts_query->have_posts() ): ?>
+   <?php while( $blog_posts_query->have_posts() ): ?>
+       <?php $blog_posts_query->the_post(); ?>
+       <div class="blog-post">
+       <?php if ( has_post_thumbnail() ) :
+           $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
+           <div class="blog-post-thumb">
+               <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
+           </div>
+       <?php endif; ?>
+       <?php the_title('<h1>', '</h1>'); ?>
+       <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
+       <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More' ); ?></a>
+       <div class="posted-in">
+           <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
+           <span><?php the_category( ', ' ); ?></span>
+       </div>
+       </div> <!-- Correct placement of closing div tag -->
+   <?php endwhile; ?>
+   <?php wp_reset_postdata(); ?>
+<?php endif; ?>
+<!-- end of custom query loop -->
 
-    </div>
+<?php the_posts_pagination(); ?>
+<?php else: ?>
+   <p><?php _e( 'No Blog Posts found', 'nd_dosth' ); ?></p>
+<?php endif; ?>
 </div>
-
-
-
-
+</div>
+</div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -39,7 +39,54 @@ get_header();
                         </a>
                         <?php echo get_avatar( get_the_author_meta('ID')  , 100 ); ?>
                     </div>
+                    <?php $category_term_list = wp_list_pluck( get_the_category(), 'slug' ); ?>
+<?php 
+    $related_posts_query = new WP_Query(
+        array(
+            'post_type' => 'post',
+            "posts_per_page" => 2,
+            'post__not_in' => array( get_the_ID() ),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'category',
+                    'terms' => $category_term_list,
+                    'field' => 'slug',
+                )
+            )
+        )   
+    );
+?>
+<?php if( $related_posts_query->have_posts() ): ?>
+    <section class="blog-posts related-articles">
+        <h2><?php _e( 'Related Articles', 'nd_dosth' ); ?></h2>
+        <?php while( $related_posts_query->have_posts() ): ?>
+            <?php $related_posts_query->the_post(); ?>
+
+
+            <div class="blog-post">
+       <?php if ( has_post_thumbnail() ) :
+           $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
+           <div class="blog-post-thumb">
+               <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
+           </div>
+       <?php endif; ?>
+       <?php the_title('<h1>', '</h1>'); ?>
+       <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
+       <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More' ); ?></a>
+       <div class="posted-in">
+           <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
+
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+    </section>
+<?php endif;  ?>
                 </div>
+
+
+
+
+
+                
                 <div id="actual-article" class="col-md-8">
                     <?php the_content(); ?>
 
@@ -47,6 +94,8 @@ get_header();
                 <?php the_excerpt(); ?>
             </div>
 
+
+            
             <?php get_template_part( 'template-parts/post', 'navigation' ); ?>
 
             <?php comments_template(); ?>
