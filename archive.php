a97697579e75a1178c4db3bb86b1829ac537ebca
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-01-30 23:42:49 +0545

    Template only for archieve

--- archive.php
+++ archive.php
@@ -10,43 +10,58 @@
 get_header();
 ?>
 
-<div class="content-container">
-    <h1 class="page-title"><?php the_archive_title(); ?></h1>    
-    <div class="container">
-        <div class="row">
-            <div class="blog-posts col-md-8">
-            <?php if ( have_posts() ): ?>
-                <?php while( have_posts() ): ?>
-                    <?php the_post(); ?>
-                    <div class="blog-post">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <?php if ( has_post_thumbnail() ) :
-                            $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
-                            <div class="blog-post-thumb">
-                                <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
-                            </div>
-                        <?php endif; ?>
-                        <?php the_excerpt(); ?>
-                        <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More', 'nd_dosth' ); ?></a>
-                        <?php $categories = get_the_category(); ?>
-                        <?php if ( ! empty( $categories ) ) : ?>
-                            <div class="posted-in">
-                                <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
-                                <a href="<?php echo get_category_link( $categories[0]->term_id ); ?>"> 
-                                    <?php echo $categories[0]->name; ?>
-                                </a>
-                            </div>
-                        <?php endif; ?>
-                    </div>
-                <?php endwhile; ?>
-                <?php the_posts_pagination(); ?>
-            <?php else: ?>
-                <p><?php _e( 'No Blog Posts found', 'nd_dosth' ); ?></p>
-            <?php endif; ?>
-            </div>
-			<?php get_sidebar(); ?>
 
+
+
+
+
+
+
+
+<div class="container">
+<?php
+$args = array(
+    'post_type' => 'post', // Specify the post type
+    'posts_per_page' => 2, // Number of posts to display
+    'orderby' => 'date', // Order posts by date
+    'order' => 'DESC', // Sort posts in descending order
+);
+
+$posts_query = new WP_Query($args);
+
+if ($posts_query->have_posts()) :
+    while ($posts_query->have_posts()) : $posts_query->the_post();
+        ?>
+        <div class="card">
+            <div class="card-header">
+                <?php
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail('medium', ['alt' => get_the_title()]);
+                }
+                ?>
+            </div>
+            <div class="card-body">
+                <span class="tag tag-teal"><?php echo get_the_category_list(', '); ?></span>
+                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                <p><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+                <div class="user">
+                    <?php echo get_avatar(get_the_author_meta('ID'), 32); ?>
+                    <div class="user-info">
+                        <h5><?php the_author(); ?></h5>
+                        <small><?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ago'; ?></small>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+
+ 
+ <?php
+    endwhile;
+    
+    wp_reset_postdata();
+    
+else :
+    echo 'No posts found.';
+endif;
+?>
 </div>
-<?php get_footer(); ?>
