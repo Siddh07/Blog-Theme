efa73922b875f340d9611a2030c30a922eb5e785
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-01-31 18:13:26 +0545

    Archieve page - frondend

--- archive.php
+++ archive.php
@@ -10,58 +10,62 @@
 get_header();
 ?>
 
-
-
-
-
-
-
-
-
 <div class="container">
-<?php
-$args = array(
-    'post_type' => 'post', // Specify the post type
-    'posts_per_page' => 2, // Number of posts to display
-    'orderby' => 'date', // Order posts by date
-    'order' => 'DESC', // Sort posts in descending order
-);
+    <?php
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 
-$posts_query = new WP_Query($args);
+    // Get the current category object
+    $current_category = get_queried_object();
 
-if ($posts_query->have_posts()) :
-    while ($posts_query->have_posts()) : $posts_query->the_post();
-        ?>
-        <div class="card">
-            <div class="card-header">
-                <?php
-                if (has_post_thumbnail()) {
-                    the_post_thumbnail('medium', ['alt' => get_the_title()]);
-                }
-                ?>
-            </div>
-            <div class="card-body">
-                <span class="tag tag-teal"><?php echo get_the_category_list(', '); ?></span>
-                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                <p><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
-                <div class="user">
-                    <?php echo get_avatar(get_the_author_meta('ID'), 32); ?>
-                    <div class="user-info">
-                        <h5><?php the_author(); ?></h5>
-                        <small><?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ago'; ?></small>
+    $args = array(
+        'post_type'      => 'post', // Specify the post type
+        'posts_per_page' => 2, // Number of posts to display
+        'orderby'        => 'date', // Order posts by date
+        'order'          => 'DESC', // Sort posts in descending order
+        'paged'          => $paged, // Added pagination parameter
+        'cat'            => $current_category->term_id, // Display posts only from the current category
+    );
+
+    $posts_query = new WP_Query( $args );
+
+    if ( $posts_query->have_posts() ) :
+        while ( $posts_query->have_posts() ) : $posts_query->the_post();
+            ?>
+            <div class="card">
+                <div class="card-header">
+                    <?php
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail( 'medium', [ 'alt' => get_the_title() ] );
+                    }
+                    ?>
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-teal"><?php echo get_the_category_list( ', ' ); ?></span>
+                    <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                    <p><?php echo wp_trim_words( get_the_excerpt(), 20 ); ?></p>
+                    <div class="user">
+                        <?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?>
+                        <div class="user-info">
+                            <h5><?php the_author(); ?></h5>
+                            <small><?php echo human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) . ' ago'; ?></small>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <?php
+        endwhile;
 
- 
- <?php
-    endwhile;
-    
-    wp_reset_postdata();
-    
-else :
-    echo 'No posts found.';
-endif;
-?>
+
+        wp_reset_postdata();
+
+    else :
+        echo 'No posts found.';
+    endif;
+    ?>
+        <?php the_posts_pagination(); ?>
+
 </div>
+
+<?php
+get_footer();
+?>
--- index.php
+++ index.php
@@ -10,98 +10,54 @@
 get_header();
 ?>
 
+<?php 
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+$blog_posts_query = new WP_Query(
+    array(
+        'post_type'      => 'post',
+        'posts_per_page' => 2,
+        'paged'          => $paged // Added pagination parameter
+    )  
+);
+?>
+
 <div class="container">
     <div class="row">
         <div class="blog-posts col-md-8">
-            <?php if ( have_posts() ): ?>
-                <?php while( have_posts() ): ?>
-                    <?php the_post(); ?>
+            <?php if ( $blog_posts_query->have_posts() ) : ?>
+                <?php while ( $blog_posts_query->have_posts() ) : $blog_posts_query->the_post(); ?>
                     <div class="blog-post">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                         <?php if ( has_post_thumbnail() ) :
                             $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
                             <div class="blog-post-thumb">
                                 <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
                             </div>
                         <?php endif; ?>
+                        <h1><?php the_title(); ?></h1>
                         <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
-                       
-                       
-                        <?php if( get_option('draft_show_readmore', true ) ): ?>
-    <a class="read-more-link" href="<?php the_permalink(); ?>">
-        <?php echo get_option( 'draft_readmore_text', __( 'Read More', 'draft' ) ); ?>
-    </a>
-<?php endif; ?>
-                       
+                      
+                        <a class="read-more-link" href="<?php the_permalink(); ?>">
+                            <?php echo get_option( 'draft_readmore_text', __( 'Read More', 'draft' ) ); ?>
+                        </a>
 
-
-<div class="posted-in">
+                        <div class="posted-in">
                             <span><?php _e( 'Posted In', 'draft' ); ?></span>
                             <span><?php the_category( ', ' ); ?></span>
                         </div>
-                    </div>
+                    </div> <!-- Correct placement of closing div tag -->
                 <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+            <!-- end of custom query loop -->
 
-                
-                
-                
-                <?php 
-$blog_posts_query = new WP_Query(
-   array(
-       'post_type'     => 'post',
-       'posts_per_page' => 2
-   )  
-);
-?>
-<?php if ( $blog_posts_query->have_posts() ): ?>
-   <?php while( $blog_posts_query->have_posts() ): ?>
-       <?php $blog_posts_query->the_post(); ?>
-       <div class="blog-post">
-       <?php if ( has_post_thumbnail() ) :
-           $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'dosth-blog-thumbnail' ); ?>
-           <div class="blog-post-thumb">
-               <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
-           </div>
-       <?php endif; ?>
-       <?php the_title('<h1>', '</h1>'); ?>
-       <?php echo custom_excerpt(20); // Display 20 words of the excerpt ?>
-      
-      
-       <a class="read-more-link" href="<?php the_permalink(); ?>">
-        <?php echo get_option( 'draft_readmore_text', __( 'Read More', 'draft' ) ); ?>
-    </a>
+            <?php the_posts_pagination(); ?>
+        </div> <!-- Closing div tag for blog-posts col-md-8 -->
 
-
-<div class="posted-in">
-           <span><?php _e( 'Posted In', 'draft' ); ?></span>
-           <span><?php the_category( ', ' ); ?></span>
-       </div>
-       </div> <!-- Correct placement of closing div tag -->
-   <?php endwhile; ?>
-   <?php wp_reset_postdata(); ?>
-<?php endif; ?>
-<!-- end of custom query loop -->
-
-<?php the_posts_pagination(); ?>
-<?php else: ?>
-   <p><?php _e( 'No Blog Posts found', 'draft' ); ?></p>
-<?php endif; ?>
-
-</div>
-<div id="blog-sidebar" class="col-md-4">
-                <?php get_sidebar(); ?>             
-            </div>
-</div>
-</div>
-
-
-
-
-
-
-
-
-
+        <div id="blog-sidebar" class="col-md-4">
+            <?php get_sidebar(); ?>             
+        </div>
+    </div> <!-- Closing div tag for row -->
+</div> <!-- Closing div tag for container -->
 
 <?php get_template_part( 'template-parts/reviews', 'slider' ); ?>
 <?php get_footer(); ?>
