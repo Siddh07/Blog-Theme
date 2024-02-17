48361674761ba1aef8710652cb1b6ae5f8c25661
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-02-17 23:57:08 +0545

    Search Result Page

--- search.php
+++ search.php
@@ -39,4 +39,69 @@ get_header();
 </div>
 </div>
 
+
+
+<div class="container">
+<div class="row ng-scope">
+    <div class="col-md-3 col-md-push-9">
+        <h4>Results <span class="fw-semi-bold">Filtering</span></h4>
+        <p class="text-muted fs-mini">Listed content is categorized by the following groups:</p>
+        <ul class="nav nav-pills nav-stacked search-result-categories mt">
+            <li><a href="#">Friends <span class="badge">34</span></a>
+            </li>
+            <li><a href="#">Pages <span class="badge">9</span></a>
+            </li>
+            <li><a href="#">Images</a>
+            </li>
+            <li><a href="#">Groups</a>
+            </li>
+            <li><a href="#">Globals <span class="badge">18</span></a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-md-9 col-md-pull-3">
+        <p class="search-results-count">About 94 700 000 (0.39 sec.) results</p>
+        <section class="search-result-item">
+            <a class="image-link" href="#"><img class="image" src="https://bootdey.com/img/Content/avatar/avatar1.png">
+            </a>
+            <div class="search-result-item-body">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <h4 class="search-result-item-heading"><a href="#">john doe</a></h4>
+                        <p class="info">New York, NY 20188</p>
+                        <p class="description">Not just usual Metro. But something bigger. Not just usual widgets, but real widgets. Not just yet another admin template, but next generation admin template.</p>
+                    </div>
+                    <div class="col-sm-3 text-align-center">
+                        <p class="value3 mt-sm">$9, 700</p>
+                        <p class="fs-mini text-muted">PER WEEK</p><a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="search-result-item">
+            <a class="image-link" href="#"><img class="image" src="https://bootdey.com/img/Content/avatar/avatar6.png">
+            </a>
+            <div class="search-result-item-body">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <h4 class="search-result-item-heading"><a href="#">john doe</a> <span class="badge bg-danger fw-normal pull-right">Best Deal!</span></h4>
+                        <p class="info">Los Angeles, NY 20188</p>
+                        <p class="description">You will never know exactly how something will go until you try it. You can think three hundred times and still have no precise result.</p>
+                    </div>
+                    <div class="col-sm-3 text-align-center">
+                        <p class="value3 mt-sm">$10, 300</p>
+                        <p class="fs-mini text-muted">PER WEEK</p><a class="btn btn-primary btn-info btn-sm" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+       
+    </div>
+</div>
+</div>
+
+
+
+
 <?php get_footer(); ?>
